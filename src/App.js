d3e0f61602d26ccf8dd2463af5748dca330330b6
Nikunj Jadav <dirensj@gmail.com>
2024-05-28 11:01:46 +0530

    added Breadcrumb  Code

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { Container, Row, Col, Button , Accordion , Card , Alert } from 'react-bootstrap';
+import { Container, Row, Col, Button , Accordion , Card , Alert , Badge , Breadcrumb } from 'react-bootstrap';
 import LoginForm from './LoginForm';
 
 
-function App() {
+const App = () =>  {
   return (
-    <div> 
-    <Alert variant="success" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a success alert which has green background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="secondary" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a secondary alert which has grey background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="primary" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a primary alert which has blue background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="danger" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a danger alert which has red background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="warning" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a warning alert which has yellow background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="light" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a light alert which has white background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="dark" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a dark alert which has dark grey background 
-      </Alert.Heading> 
-    </Alert> 
-
-    <Alert variant="info" style={{ width: "42rem" }}> 
-      <Alert.Heading> 
-        This is a info alert which has light blue background 
-      </Alert.Heading> 
-    </Alert> 
-  </div> 
+    <>
+      <div style={{ display: 'block',  
+                  width: 700, padding: 30 }}> 
+      <h4>React-Bootstrap Breadcrumb Component</h4> 
+      <Breadcrumb> 
+        <Breadcrumb.Item href="#"> 
+           Dashboard 
+        </Breadcrumb.Item> 
+        <Breadcrumb.Item href="#"> 
+          Profile 
+        </Breadcrumb.Item> 
+        <Breadcrumb.Item active> 
+          Details 
+        </Breadcrumb.Item> 
+      </Breadcrumb> 
+    </div> 
+    </>
   );
 }
 
