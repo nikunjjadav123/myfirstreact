be1ee726ee4d91c19360d526da3d3db8e8637480
Nikunj Jadav <dirensj@gmail.com>
2024-05-28 18:56:15 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import PageNotAvailable from './Pages/PageNotAvailable'
 function App() {
    return (
       <>
-         <h1 className="text-3xl font-bold underline grid-cols-1	">
+         <h1 className="text-3xl font-bold underline">
             Hello world!
          </h1>
      
