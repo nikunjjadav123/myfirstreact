ae4c28b7e1a1d53e6a66c5fc05ca01ca196b1cd1
Nikunj Jadav <dirensj@gmail.com>
2024-06-02 18:06:51 +0530

    How to use useState in arrow function instead of hook

--- src/App.js
+++ src/App.js
@@ -1,47 +1,31 @@
-import React from "react";
+import React, { useState } from 'react'
 
-class App extends React.Component {
-	state = {
-		value: 10,
-	};
+const App = () => { 
+const [count, setCount] = useState(0); 
+	
+const increaseCount = () => { 
+	setCount(count + 1); 
+} 
+const decreaseCount = () => { 
+	setCount(count - 1); 
+} 
+	
+return ( 
+	<div> 
+	<h1>Countdown Information</h1> 
+	
+<p>Value is {count}</p> 
 
-	onValueChange = (event) => {
-		this.setState({ value: event.target.value });
-	};
+	
+	<button onClick={increaseCount}> 
+		Increment 
+	</button> 
 
-	render() {
-		return (
-			<div
-				style={{
-					textAlign: "center",
-					margin: "auto",
-				}}
-			>
-				<h1 style={{ color: "green" }}>
-					GeeksforGeeks
-				</h1>
-				<h3>
-					React Example for Input Box with floatin
-					number
-				</h3>
-				<label> Floating Number: </label>
-				<input
-					type="number"
-					step="0.1"
-					min="0"
-					max="20"
-					value={this.state.value}
-					onChange={(event) =>
-						this.onValueChange(event)
-					}
-				/>
-				<h4>
-					Current input value is
-					{this.state.value}
-				</h4>
-			</div>
-		);
-	}
-}
+	<button onClick={decreaseCount}> 
+		Decrement 
+	</button> 
+	</div> 
+) 
+} 
 
-export default App;
+export default App 
