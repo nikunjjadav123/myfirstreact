fb766f83fca126890415e6c36fb6d451f93ea583
Nikunj Jadav <dirensj@gmail.com>
2024-06-05 12:12:21 +0530

    Export Import React JS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import NetflixSeries,{Header,Footer} from './Pages/NetflixSeries';
 
 const App = () => { 
-const [count, setCount] = useState(0); 
-	
-const increaseCount = () => { 
-	setCount(count + 1); 
-} 
-const decreaseCount = () => { 
-	setCount(count - 1); 
-} 
-	
-return ( 
-	<div> 
-	<h1>Countdown Information</h1> 
-	
-<p>Value is {count}</p> 
-
-	
-	<button onClick={increaseCount}> 
-		Increment 
-	</button> 
-
-	<button onClick={decreaseCount}> 
-		Decrement 
-	</button> 
-	</div> 
-) 
+	return(
+		<>
+			<div>
+				<Header />
+				<NetflixSeries />
+				<Footer />
+			</div>
+		</>
+		
+	)
 } 
 
 export default App 
---
+++ src/Pages/NetflixSeries.js
@@ -0,0 +1,14 @@
+
+export const Header = () => {
+    return <p>Header</p>
+}
+
+const NetflixSeries = () => {
+    const name="Rahul";
+    return name;
+}
+export default NetflixSeries;
+
+export const Footer = () => {
+    return <p>Copyright 2024</p>
+}
