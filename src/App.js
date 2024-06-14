4ba76eec91112ef13daab617164600c562a69854
Nikunj Jadav <dirensj@gmail.com>
2024-06-14 17:41:13 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+<<<<<<< HEAD
 import NetflixSeries,{Header,Footer} from './Pages/NetflixSeries';
 import LoadUserData from './Pages/LoadUserData';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -14,6 +15,29 @@ const App = () => {
 				</Card>
 				<LoadUserData />
 			</div>
+=======
+import {BrowserRouter as Router,Route, Link, BrowserRouter, Routes} from "react-router-dom";
+import SimpleNotes from './Components/SimpleNotes';
+import UserData from './Components/UserData';
+import Layout from './Components/Layout';
+import "./Components/css/style.css";
+const App = () => { 
+	return(
+		<>
+			<div className="App">
+				<header className="App-header">
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<Layout />}>
+								<Route index element={<UserData />} />
+								<Route path="simple-notes" element={<SimpleNotes />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</header>
+			</div>
+			
+>>>>>>> c6587a6 (added)
 		</>
 		
 	)
